1e4e975a6c346bb6e9f2b85a7e9dbe7cd55bb372
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-05 18:38:32 +0530

    this is 2nd commit

--- ex1.py
+++ ex1.py
@@ -1,4 +1,6 @@
+# Now first we are practicing 
 print ( "hello world")
 print ("hello sanjeev")
 print ("move into the python world")
-print ("adding new code") 
+print ("adding new code")
+# we have done first exercise
