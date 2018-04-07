0a32c4912aad10a7a25198eb6e6707f6669967a1
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-07 23:00:01 +0530

    code is defined for string variable

---
+++ ex5_var_with_string_dcl.py
@@ -0,0 +1,21 @@
+name ='sanjeev kumar'
+age=27
+height=170 #in inches
+weight=64 #in kg
+eyes="blue"
+teeth="white"
+hair='black'
+
+#print the text using variable
+
+print(f"Let's talk about {name}.")
+print(f"he's {height} inches tall.")
+print(f"he's {weight} kilogram normal ")
+print("Actually that's not too heavy .")
+print(f"he's got {eyes} eyes and {hair} hair.")
+print(f"His teeth are usually {teeth} depending on the coffee")
+
+#this line is litle bit tricky so try to get it exactly right
+
+total =age+height+weight
+print(f'If I add {age},{height},{weight} I get {total}.')
