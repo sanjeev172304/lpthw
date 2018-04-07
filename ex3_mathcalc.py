11ffb92b369d8da694fa988f51bcf5d6c0df3e8b
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-07 21:48:07 +0530

    this exercise doing math calc for count

---
+++ ex3_mathcalc.py
@@ -0,0 +1,24 @@
+# this code managing the math calc for counting the chickens.
+print("i will now counts my chickens:")
+print("Hens", 25 + 30 / 6)
+print("Roosters",100-25*3%4)
+
+
+# now counting eggs.
+print("Now I will count the eggs:")
+print(3+2+1-5+4%2-1/4+6)
+
+
+print("is it true that 3+2<5-7?")
+
+print(3+2<5-7)
+
+print("what is 3+2?",3+2)
+print ("what is 5-7?",5-7)
+
+print("oh, thst's why it's false.")
+
+print("how about some more.")
+
+print("Is it greater or equel:",5>=-2)
+print("Is it Less or equel:",5<=-2)
