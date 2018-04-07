70479dc37edbf19e91618dd008e3ba46724ad2b2
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-07 21:02:51 +0530

    This is print fn statement

---
+++ ex1_print_fn.py
@@ -0,0 +1,8 @@
+#This is Print statement for display the content inside called statement 
+print("Hello World!")
+print("Hello Again")
+print("I like typing this.")
+print("This is fun.")
+print('Yay! Printing.')
+print("I'd much rather you 'not'.")
+print('I "said" do not touch this.')
