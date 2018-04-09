dd8f281bcdc4ea4564ecffde6eeddb0c274a360e
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-09 11:31:20 +0530

    here in triple double qoute

---
+++ x9_string_print.py
@@ -0,0 +1,12 @@
+#Here's some new strange stuff, remember type it exactly.
+days="Mon tue wed Thu Fri Sat Sun"
+months="jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+print("here are the days:",days)
+print("Here are the months:", months)
+
+print("""There's something going on here. 
+	With the three double-quotes.
+	we'll be able to typeas much as we like.
+	Even 4 lines if we want, or 5, or 6"""
+
+)
