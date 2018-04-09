f4b1709f529fca56aacad5e1592ceae77e59f6e8
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-09 11:45:37 +0530

    here we use escape with double and singl quote

---
+++ ex10.escape.py
@@ -0,0 +1,18 @@
+#here printing the string using with escape character.
+tabby_cat="\tI'm tabbed in"
+persian_cat="I'm Split\non a line."
+backslash_cat="I'm\\a\\cat."
+
+fat_cat="""I'll do a list:
+\t* cat food
+\t* cat food
+\t* fishies
+\t*catnip\n\t* Grass
+
+ """
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
+
