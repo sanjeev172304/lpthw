ead1cdeb1f1fd3f1f4366331b71f03d94645469b
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-09 12:11:22 +0530

    code for take a input from users

---
+++ ex11_input.py
@@ -0,0 +1,9 @@
+print("how old are you?")
+age=input()
+print("how tall are you?")
+height=input()
+print("how much do you weight?")
+weight=input()
+
+
+print ("So, you're %r old, %r tall, and %r heavy."%(age,height,weight))
