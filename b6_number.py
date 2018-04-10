e45363893fff89a54e8c2d9f01c36f1ba3cfbb82
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-10 11:06:59 +0530

    Number

---
+++ b6_number.py
@@ -0,0 +1,15 @@
+#number - int float and complex number
+
+a=5
+#output:<class 'int'>
+print(type(a))
+
+#output: <class 'float'>
+print(type(5.0))
+
+#output: (8+3j)
+c=5+3j
+print(c+3)
+
+#output:true
+print(isinstance(c, complex))
