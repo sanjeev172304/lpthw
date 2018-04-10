679d72c47091fba3f96ad0748c8ead4c946890e1
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-10 11:15:44 +0530

    single list and nested list

---
+++ b7_list.py
@@ -0,0 +1,26 @@
+my_list=['p','r','o','b','e']
+
+#output p
+print(my_list[0])
+
+#output o
+print(my_list[2])
+
+#output e
+print(my_list[5-1])
+
+#error! only integer can be used for indexing
+#my_list[4.0]
+
+#Nested list
+
+n_list=["happy",[2,0,1,5]]
+
+#Nested indexing
+
+#output a
+print(n_list[0][1])
+
+#output 5
+print(n_list[1][3])
+
