62c5a1d966fab3085796cfbc5d0576c2dbe48e6e
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-11 06:04:58 +0530

    Dictionary

---
+++ b8_dictionary.py
@@ -0,0 +1,12 @@
+my_dict={'name':'jack','age':26}
+
+#output: jack
+print(my_dict['name'])
+
+#output:26
+print(my_dict.get('age'))
+
+#trying to access keys which does not exist thron error
+
+#my_dict.get(''address')
+#my_dict['address']
