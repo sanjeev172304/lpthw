94099597aea26e779c06fbe8d76dc000e106f4b0
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-12 12:07:07 +0530

    swapping value using tem var

---
+++ Excercise_program/p6_Temp_var.py
@@ -0,0 +1,10 @@
+var1=int(input("enter first var="))
+var2=int(input("enter second va="))
+
+temp_var=var1
+var1=var2
+var2=temp_var
+
+print("the value of x after swapping:{}".format(var1))
+
+print('the value of y after swapping:{}'.format(var2))
