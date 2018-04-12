422b4bd9c5f28b908cd30f6ce1781fb3992d31a9
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-12 12:40:06 +0530

    cal celsius and call fahrenheit

---
+++ Excercise_program/p9_temp.py
@@ -0,0 +1,15 @@
+#convrting celsius to fahrenheit
+celsius_f=float(input("enter the celsius value"))
+
+fahrenheit_cal=(celsius_f*1.8)+32
+
+print('%0.1f degree celsius is to %0.1f degree fahrenheit'%(celsius_f,fahrenheit_cal))
+
+# fahrenheit to celsius
+
+fahrenheit_c=float(input("enter the celsius value="))
+
+celsius_cal=(fahrenheit_c-32)/1.8
+
+print("%0.1f degree fahrenheit to %0.1f degree celsius"%(fahrenheit_c,celsius_cal))
+
