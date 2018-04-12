5f086f97275511839dd14e3eb8b0ea003ea7590d
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-12 14:56:38 +0530

    leap year

---
+++ Excercise_program/p11_leap_year.py
@@ -0,0 +1,13 @@
+year = int(input("Enter the Year:"))
+
+if(year%4)==0:
+  if(year%100)==0:
+    if(year%400)==0:
+       print("{0} is leap year".format(year))
+    else:
+       print("{0} is not leap year".format(year))
+  else:
+     print("{0} is leap year".format(year))
+else:
+   print("{0} is not leap year".format(year))
+	
