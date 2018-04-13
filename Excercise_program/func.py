4c9897f6ba9137efea937adab6769a8871d2b043
sanjeevkr17 <sanjeev.kumar@sysarks.com>
2018-04-13 11:13:36 +0530

    function

---
+++ Excercise_program/func.py
@@ -0,0 +1,14 @@
+def myfun(n1,n2):
+	"""this function greate
+	the person with the provided message"""
+	print("hello",n1,n2)
+myfun(3,4)
+
+first_name=input("enter name")
+last_name=input("last name")
+def myname(first_nm, last_nm)
+
+	a=first_nm
+	b==last_nm
+        print("your first name is",+a,"and Last name is",+b)
+myname(first_nm, last_nm)
